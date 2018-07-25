ea4224b3ec5f8ae0c69b3c71dd382451423e1f02
unknown <shayaansalimsa@gmail.com>
2018-07-25 13:58:44 -0700

    Exercise 3.3

--- src/complex/complex.cpp
+++ src/complex/complex.cpp
@@ -3,23 +3,47 @@
 
 #include <iostream>	// contains the definition of cout, endl
 #include <complex>	// contains the complex class definition
+#include <cmath>
 
 using namespace std; // cout, endl, complex are all part of this namespace
 typedef complex<float> complexnumb;
 int main()
 {
     
-	auto num1 = complexnumb{2.0, 2.0};  // use auto for type deduction
-	auto num2 = complexnumb{4.0, -2.0}; // use uniform initialisation syntax (curly braces)
 
-	auto answer = num1 * num2; // type deduced for 'answer' is: complex<float>
-
-	cout << "The answer is: " << answer << endl;
-	cout << "Or in a more familiar form: " << answer.real() 
-			<< " + " << answer.imag() << "j" 
-			<< endl	<< endl;
-
-	// answer++;
-/*Because the operator is not overloaded. The compiler does not know what to do with the ++ */
+    float a,b,c,sq,x1,x2,y1,y2;
+    char q;
+    while(q!='q'){
+    
+    cin>>q;
+    if(q=='q'){
+        break;
+    }
+    
+    else {
+        cin>>b>>c;
+        a=q-48;
+        cout<<a<<endl;
+    sq=b*b-4*a*c;
+    
+    if (sq>=0){
+    x1 = ((-b+sqrt(sq))/(2*a));
+    y1=0;
+    y2=0;
+    x2 = ((-b-sqrt(sq))/(2*a));
+    }
+    
+    else {
+    x1 = (-b/(2*a));
+    y1 =sqrt(-sq)/(2*a);
+    x2 = (-b/(2*a));
+    y2 = -sqrt(-sq)/(2*a);
+    }
+    cout<<x1<<" "<<y1<<endl;
+    cout<<x2<<" "<<y2<<endl;
+    }
+    }
+    
 	return 0;
+    
 }
